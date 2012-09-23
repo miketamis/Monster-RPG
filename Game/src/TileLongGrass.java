fb7f45509e1dc26c2b88ae77c4a9c1fc5f333a79
Jareth MacArthur <jtmaca@gmail.com>
2012-09-23 19:55:24 +1200

    minor fix in rolling for monsters

--- Game/src/TileLongGrass.java
+++ Game/src/TileLongGrass.java
@@ -2,14 +2,17 @@ import java.util.Random;
 
 
 public class TileLongGrass extends Tile {
-	
- 
-	
+
+public static final int CHANCE_OF_FINDING_MONSTER = 2;
+
 	@Override
 	public void stepOn(EntityPlayer entityPlayer, int x, int y) {
 		System.out.println("YOU DARE STAND ON LONG GRASS!!!!" );
 		Random generator = new Random();
-		 int identify = generator.nextInt(3);
-		MonsterData.monsters[identify].printMonster();
+
+                if(generator.nextInt(CHANCE_OF_FINDING_MONSTER) == 0)
+		MonsterData.monsters[generator.nextInt(MonsterData.monsters.length)].printMonster();
+                else
+                System.out.println("No monster");
 	}
 }
