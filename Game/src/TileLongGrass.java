3299a16f1ddd54bfed0a385bef08e767828a86ba
Jareth MacArthur <jtmaca@gmail.com>
2012-09-23 18:12:28 +1200

    .

--- Game/src/TileLongGrass.java
+++ Game/src/TileLongGrass.java
@@ -2,7 +2,6 @@
 public class TileLongGrass extends Tile {
 	@Override
 	public void stepOn(EntityPlayer entityPlayer, int x, int y) {
-		System.out.println("YOU DARE STAND ON LONG GRASS!!!!");
-		
+		System.out.println("YOU DARE STAND ON LONG GRASS!!!!" );
 	}
 }
