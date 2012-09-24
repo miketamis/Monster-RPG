fa2850249d077636a414731b9b39eb1a4a1ca331
Jareth MacArthur <jtmaca@gmail.com>
2012-09-24 14:36:09 +1200

    Added new monster string variables(school)
    may not be 100% working

--- Game/src/Monster.java
+++ Game/src/Monster.java
@@ -1,13 +1,20 @@
 public class Monster{
 	String name;
-	
-	public Monster(String name){
+         String type;
+         int tier;	
+	public Monster(String name,String Type,int Tier){
+                  type = Type;
+                  tier = Tier;
 		this.name = name;
 	}
 	
 
 public void printMonster(){
 	      System.out.println("Found:"+ name );
+	      System.out.println("Type:"+  type);
+	      System.out.println("Tier:"+ tier);
+
 }
 	
 }
+
