a94f5ec8aa4570b3c3dcce0e2df97e8f87de625e
Jareth MacArthur <jtmaca@gmail.com>
2012-09-24 15:53:19 +1200

    Made neater console post

--- Game/src/TileLongGrass.java
+++ Game/src/TileLongGrass.java
@@ -7,7 +7,7 @@ public static final int CHANCE_OF_FINDING_MONSTER = 2;
 
 	@Override
 	public void stepOn(EntityPlayer entityPlayer, int x, int y) {
-		System.out.println("-LONG GRASS MONSTER ROLL-" );
+		System.out.println("----------------" );
 		Random generator = new Random();
 
                 if(generator.nextInt(CHANCE_OF_FINDING_MONSTER) == 0)
