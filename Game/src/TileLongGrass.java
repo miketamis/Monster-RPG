c7de74610237e6ef43f19ed2c1c7e47c708d2248
Jareth MacArthur <jtmaca@gmail.com>
2012-09-25 19:40:59 +1200

    Wild monsters are now generated.

--- Game/src/TileLongGrass.java
+++ Game/src/TileLongGrass.java
@@ -9,10 +9,21 @@ public static final int CHANCE_OF_FINDING_MONSTER = 2;
 	public void stepOn(EntityPlayer entityPlayer, int x, int y) {
 		System.out.println("----------------" );
 		Random generator = new Random();
-
-                if(generator.nextInt(CHANCE_OF_FINDING_MONSTER) == 0)
-		MonsterData.monsters[generator.nextInt(MonsterData.monsters.length)].printMonster();
-                else
+		int Identify = (generator.nextInt(MonsterData.monsters.length));
+		
+                if(generator.nextInt(CHANCE_OF_FINDING_MONSTER) == 0){
+		         MonsterData.monsters[Identify].printMonster();
+                 CreateWildMonster(MonsterData.monsters[Identify]);               
+                 }
+                 else
                 System.out.println("No monster");
+               
+	}
+
+	private void CreateWildMonster(Monster monster) {
+		Random generator = new Random();
+		int lvl = generator.nextInt(10);
+		WildMonster wild = new WildMonster(monster.name, monster.type, monster.tier, lvl);
+		wild.printWildMonster();
 	}
 }
---
+++ Game/src/WildMonster.java
@@ -0,0 +1,20 @@
+
+public class WildMonster {
+String name;
+String type;
+int tier;
+int lvl;
+public WildMonster(String Name, String Type, int Tier, int Lvl) {
+	this.name = Name;
+	this.type = Type;
+	this.tier = Tier;
+	this.lvl  = Lvl;
+		// TODO Auto-generated constructor stub
+	}
+public void printWildMonster(){
+    System.out.println("GENERATED WILD MONSTER:");
+    System.out.println("Name:"+ name );
+    System.out.println("Type:"+  type);
+    System.out.println("Lvl:"+ lvl);
+    
+}}
