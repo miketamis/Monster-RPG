7defa3b4603078866570fa08e0c7d4316a69b1c5
miketamis <tamis.mike@gmail.com>
2012-09-26 18:36:19 +1200

    added first steps to storing caught monsters

--- Game/src/PlayerMonsterStore.java
+++ Game/src/PlayerMonsterStore.java
@@ -1,8 +1,15 @@
-
 public class PlayerMonsterStore {
-	public static void catchWildMonster(WildMonster monster){
+	CaughtMonster[] monsters;
+	public PlayerMonsterStore()
+	{
+		monsters = new CaughtMonster[6];
+	
+	}
+	public void catchWildMonster(WildMonster monster){
 		int xp = 0;
 		TileLongGrass.available = false;
 		CaughtMonster caught = new CaughtMonster(monster.name, monster.type, monster.tier, monster.lvl, monster.hp, monster.atk, monster.def, monster.spd, xp);
-}
+		monsters[0] = caught;
+
+	}
 }
