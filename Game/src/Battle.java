506cdd8573aee1e0cd7638799c30d3db8d7271fa
Jareth MacArthur <jtmaca@gmail.com>
2012-09-26 21:29:25 +1200

    Small changes

--- Game/src/Battle.java
+++ Game/src/Battle.java
@@ -2,13 +2,13 @@
 public class Battle {
 	static int  WIDTH = 5;
 	static int HEIGHT = 5;
-	Tile[][]tiles = new Tile[WIDTH][HEIGHT];
+	HexTile[][]hextiles = new HexTile[WIDTH][HEIGHT];
 	
 	 public Battle(){
 		for(int x = 0; x > WIDTH; x++)
 		{
 			for(int y = 0; y > HEIGHT; y++){
-				tiles[x][y] = new Tile(x, y, x % 2 == 0);
+				hextiles[x][y] = new HexTile(x, y, x % 2 == 0);
 				
 			}
 		}
